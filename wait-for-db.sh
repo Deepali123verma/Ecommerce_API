e73bab70a1844eb106bc418e33b7ab7175f65dc2
Deepali123verma <166192241+Deepali123verma@users.noreply.github.com>
2025-10-31 16:12:05 +0530

    Update wait-for-db.sh for new DB host and command

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
-
-# Arguments (like uvicorn command)
-CMD="$@"
-
-# Database host and port
-DB_HOST=${DB_HOST:-db}
+DB_HOST=${DB_HOST:-ecommerce-db.internal}
 DB_PORT=${DB_PORT:-5432}
-
 echo "⏳ Waiting for PostgreSQL at $DB_HOST:$DB_PORT..."
-
-# Check every 2 seconds until DB is ready
 while ! nc -z $DB_HOST $DB_PORT; do
+  echo "Postgres is unavailable - sleeping"
   sleep 2
 done
-
-echo "✅ PostgreSQL is ready! Starting the application..."
-exec $CMD
+echo "✅ PostgreSQL is up - starting FastAPI"
+exec "$@"
